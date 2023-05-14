6255507a83a911a91ef0a2784b1148ef8efeb079
aaavalenca <32614967+aaavalenca@users.noreply.github.com>
2023-05-13 21:32:12 -0300

    algorithm for finding rhyme semi-completed

--- Crawler/manipulate_database.py
+++ Crawler/manipulate_database.py
@@ -10,10 +10,10 @@ def not_in_database(word):
     return not ((df['palavra'].eq(word)).any())
 
 def merge_database():
-    list_dir = sorted(os.listdir("Crawler/rimas"))
+    list_dir = sorted(os.listdir("Crawler/labeled"))
     df = pd.DataFrame()
     for filename in list_dir:
-        ndf = pd.read_csv("Crawler/rimas/" + filename, delimiter= ",")
+        ndf = pd.read_csv("Crawler/labeled/" + filename, delimiter= ",")
         df = pd.concat([df, clean(ndf)], axis=0, ignore_index=True)
     os.makedirs('Crawler/database', exist_ok=True) 
     df.to_csv("Crawler/database/database.csv")
--- Crawler/rimar.py
+++ Crawler/rimar.py
@@ -1,20 +1,32 @@
-import csv
 import pandas as pd
-import ast
+import unicodedata
 
-def read_rhymes(letter):
-    df = pd.read_csv("Crawler/labeled/" + letter + ".csv", converters={'divisao_list' : eval, 'fonetica_list' : eval})
+def read_rhymes():
+    df = pd.read_csv("Crawler/database/database.csv", converters={'divisao_list' : eval, 'fonetica_list' : eval})
     df2 = df[['palavra', 'fonetica', 'tonica', 'antepenultima', 'penultima', 'ultima']]
     x = input("o que você quer rimar?: ")
     row = df2[df2['palavra'] == x]
     row_stripped = row.loc[row.index[0]]
     tonica_pos = row_stripped["tonica"]
-    tonica = row_stripped[tonica_pos]
+    tonica = row_stripped[tonica_pos].split('ˈ')[-1]
+    print(tonica_pos, tonica)
+    df2.fillna('', inplace=True)
+    df2 = df2[df['tonica'] == tonica_pos]
+    selected_rows = []
+    tonica = unicodedata.normalize('NFC', tonica)
 
-    print(tonica, tonica_pos)
+    for index, row in df2.iterrows():
+        col = row['tonica']
+        if col == '':
+            continue
+        val = row[col]
+        val = unicodedata.normalize('NFC', val)
+        if not (tonica in val):
+            continue
+        selected_rows.append(row)
 
-    print(df2[df2[tonica_pos] == tonica])
+    selected_df = pd.DataFrame(selected_rows)
+    print(selected_df)
 
 
-
-read_rhymes("a")
+read_rhymes()
