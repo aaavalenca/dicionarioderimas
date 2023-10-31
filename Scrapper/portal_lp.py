5b3e2a7aac416f85d159ac6a3bc65fc145663cb4
aaavalenca <32614967+aaavalenca@users.noreply.github.com>
2023-10-31 15:59:22 -0300

    refactored folders

---
+++ Scrapper/portal_lp.py
@@ -0,0 +1,30 @@
+import scrapper
+import os
+import csv
+
+def bfs(letter):
+    url = "http://www.portaldalinguaportuguesa.org"
+    suffix = "/index.php?action=fonetica&region=rjo&act=list&letter=" + letter
+    final = []
+    full_link = url+suffix
+
+    # create_project_dir(folder)
+    final = scrapper.get_entries_page(full_link)
+    search = False
+    seguinte, search = scrapper.find_seguintes(url, full_link)
+    while search:
+        final.extend(scrapper.get_entries_page(seguinte))
+        seguinte, search = scrapper.find_seguintes(url, seguinte)
+    print(final[0])
+
+    fields = ['palavra', 'divisao_list', 'divisao', 'categoria', 'fonetica_list', 'fonetica', 'tonica_num', 'tonica', 'antepenultima', 'penultima', 'ultima']
+
+    with open("Crawler/labeled/" + letter + ".csv", 'w') as f:
+        write = csv.writer(f)
+        write.writerow(fields)
+        write.writerows(final)
+
+# uma pasta para cada projeto
+def create_project_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
