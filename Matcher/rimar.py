d7651bf102612db8e147646174329e441910a2e3
aaavalenca <32614967+aaavalenca@users.noreply.github.com>
2023-11-04 18:13:42 -0300

    flexible rhymes

--- Matcher/rimar.py
+++ Matcher/rimar.py
@@ -8,22 +8,34 @@ def encontrar_palavra(w, df):
     w = w.lower()
     return difflib.get_close_matches(w, df.palavra.astype(str), n=50, cutoff=.6)
 
-def tratar_fonema(fonema):
-    fonema = unicodedata.normalize('NFC', fonema)
-    # se for tônica, tirar indicador
-    fonema = fonema.split('ˈ')[-1]
-    # verificar a lista de consoantes
-    lista_de_consoantes = ['tˌ', 'mw', 'k', 'gwˌ', 'spɾ', 'rj', 'ʎˌ', 'lˌ', 'ɦ', 'blˌ', 'pˌ', 'pɾ', 't', 'zj', 'zˌ', 'bɾj', 'glj', 'tʃj', 'kɾˌ', 'ʎ', 'plj', 'sw', 'bɾ', 'vɾ', 'f', 'ɲ', 'ft', 'tsj', 'x', 'gɾˌ', 'bl', 'klj', 'v', 's', 'p', 'z', 'gˌ', 'fj', 'bj', 'kz', 'ɦw', 'kɾw', 'kɾ', 'w', 'fɾ', 'fˌ', 'tʃˌ', 'g', 'gɾ', 'tɾ', 'pj', 'ɾj', 'dɾ', 'gɾw', 'ɾw', 'mˌ', 'fl', 'ng', 'ps', 'kɾj', 'dɾw', 'tɾj', 'vj', 'dw', 'bˌ', 'tɾˌ', 'ks', 'gj', 'pl', 'ʃj', 'sˌ', 'ɦˌ', 'm', 'tʃ', 'blj', 'pɾw', 'dɾj', 'zw', 'l', 'd', 'lj', 'pɾˌ', 'mj', 'sk', 'kl', 'ˌ', 'st', 'kw', 'j', 'ts', 'kˌ', 'nj', 'ms', 'nˌ', 'bw', 'ɦj', 'dˌ', 'r', 'pw', 'fɾˌ', 'ɾ', 'sj', 'ʃ', 'gw', 'tsˌ', 'cr', 'b', 'nw', 'plˌ', 'ɾˌ', 'kj', 'lw', 'pɾj', 'n', 'fɾj', 'vˌ', 'gl', 'tl']
-    for elem in lista_de_consoantes:
-        if fonema.startswith(elem):
-            fonema = fonema.replace(elem, '')
-            continue
-    return fonema
+def tratar_fonemas(divisao_fonetica):
+    divisao_fonetica_tratada = []
+    for fonema in divisao_fonetica:
+        fonema = unicodedata.normalize('NFC', fonema)
+        # se for tônica, tirar indicador
+        fonema = fonema.split('ˈ')[-1]
+        # verificar a lista de consoantes
+        lista_de_consoantes = ['tˌ', 'mw', 'k', 'gwˌ', 'spɾ', 'rj', 'ʎˌ', 'lˌ', 'ɦ', 'blˌ', 'pˌ', 'pɾ', 't', 'zj', 'zˌ', 'bɾj', 'glj', 'tʃj', 'kɾˌ', 'ʎ', 'plj', 'sw', 'bɾ', 'vɾ', 'f', 'ɲ', 'ft', 'tsj', 'x', 'gɾˌ', 'bl', 'klj', 'v', 's', 'p', 'z', 'gˌ', 'fj', 'bj', 'kz', 'ɦw', 'kɾw', 'kɾ', 'w', 'fɾ', 'fˌ', 'tʃˌ', 'g', 'gɾ', 'tɾ', 'pj', 'ɾj', 'dɾ', 'gɾw', 'ɾw', 'mˌ', 'fl', 'ng', 'ps', 'kɾj', 'dɾw', 'tɾj', 'vj', 'dw', 'bˌ', 'tɾˌ', 'ks', 'gj', 'pl', 'ʃj', 'sˌ', 'ɦˌ', 'm', 'tʃ', 'blj', 'pɾw', 'dɾj', 'zw', 'l', 'd', 'lj', 'pɾˌ', 'mj', 'sk', 'kl', 'ˌ', 'st', 'kw', 'j', 'ts', 'kˌ', 'nj', 'ms', 'nˌ', 'bw', 'ɦj', 'dˌ', 'r', 'pw', 'fɾˌ', 'ɾ', 'sj', 'ʃ', 'gw', 'tsˌ', 'cr', 'b', 'nw', 'plˌ', 'ɾˌ', 'kj', 'lw', 'pɾj', 'n', 'fɾj', 'vˌ', 'gl', 'tl']
+        for elem in lista_de_consoantes:
+            if fonema.startswith(elem):
+                fonema = fonema.replace(elem, '')
+                continue
+            if 'h' in fonema:
+                fonema = fonema.replace('h', 'ɦ')
+        divisao_fonetica_tratada.append(fonema)
+    return divisao_fonetica_tratada
 
 def checar_rima(divisao_fonetica, tonica_fonetica_num, df):
     selected_rows = []
+    divisao_fonetica = tratar_fonemas(divisao_fonetica)
+
     for index, row in df.iterrows():
-        if row['divisao_fonetica'][tonica_fonetica_num:] == divisao_fonetica[tonica_fonetica_num:]:
+        divisao_fonetica_row = tratar_fonemas(row['divisao_fonetica'])
+
+        if row['palavra'] == 'tártaro':
+            print(divisao_fonetica_row)
+
+        if divisao_fonetica_row[tonica_fonetica_num:] == divisao_fonetica[tonica_fonetica_num:]:
             selected_rows.append(row)
     return selected_rows
 
@@ -55,27 +67,8 @@ def ler_rimas():
     # pega posição da tônica
     tonica_fonetica_num = row_stripped["tonica_fonetica_num"]
 
-    # extrai a tônica
-    tonica = divisao_fonetica[tonica_fonetica_num]
-    
-    # pega a tônica de acordo com sua posição, retira o indicador de tônica
-    tonica = tratar_fonema(tonica)
-
-    penultima = ""
-
-    # extrai a penúltima
-    if tonica_fonetica_num < -1:
-        penultima = tratar_fonema(divisao_fonetica[-2])
-
-    # # extrai a última
-    ultima = tratar_fonema(divisao_fonetica[-1])
-
-    # com a posição da tônica, sabemos se é oxítona, paroxítona,
-    # ou proparoxítona; filtramos o df de acordo
-    df = df[df['tonica_fonetica_num'] == tonica_fonetica_num]
-
     # selected_rows = checar_rima(tonica, penultima, ultima, df)
-    selected_rows = checar_rima(divisao_fonetica, tonica_fonetica_num, df)
+    selected_rows = checar_rima(divisao_fonetica, tonica_fonetica_num, df[df['tonica_fonetica_num'] == tonica_fonetica_num])
 
     selected_df = pd.DataFrame(selected_rows)
     print(selected_df[['palavra', 'divisao_fonetica']])
