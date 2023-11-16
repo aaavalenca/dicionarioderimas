7730f8cc3db20501d2fbfe373e269369b3fe5a1f
aaavalenca <32614967+aaavalenca@users.noreply.github.com>
2023-11-16 11:11:09 -0300

    made simple fonemes a list

--- Scrapper/PortalLP/make_vowels.py
+++ Scrapper/PortalLP/make_vowels.py
@@ -32,13 +32,13 @@ def fonema_simple(fonema):
         return fonema
 
 def tratar_fonemas(divisao_fonetica):
-    divisao_fonetica_tratada = ''
+    divisao_fonetica_tratada = []
     for fonema in divisao_fonetica:
         fonema = unicodedata.normalize('NFC', fonema).replace('ɦ', 'h')
         # se for tônica, tirar indicador
         if 'ˈ' in fonema:
             fonema = fonema.split('ˈ')[-1]
-            fonema = '*' + fonema
+            divisao_fonetica_tratada.append('*')
 
         # verificar a lista de consoantes
         lista_de_consoantes = ['tˌ', 'mw', 'k', 'gwˌ', 'spɾ', 'rj', 'ʎˌ', 'lˌ', 'h', 'ɦ', 'blˌ', 'pˌ', 'pɾ', 't', 'zj', 'zˌ', 'bɾj', 'glj', 'tʃj', 'kɾˌ', 'ʎ', 'plj', 'sw', 'bɾ', 'vɾ', 'f', 'ɲ', 'ft', 'tsj', 'x', 'gɾˌ', 'bl', 'klj', 'v', 's', 'p', 'z', 'gˌ', 'fj', 'bj', 'kz', 'ɦw', 'kɾw', 'kɾ', 'w', 'fɾ', 'fˌ', 'tʃˌ', 'g', 'gɾ', 'tɾ', 'pj', 'ɾj', 'dɾ', 'gɾw', 'ɾw', 'mˌ', 'fl', 'ng', 'ps', 'kɾj', 'dɾw', 'tɾj', 'vj', 'dw', 'bˌ', 'tɾˌ', 'ks', 'gj', 'pl', 'ʃj', 'sˌ', 'ɦˌ', 'm', 'tʃ', 'blj', 'pɾw', 'dɾj', 'zw', 'l', 'd', 'lj', 'pɾˌ', 'mj', 'sk', 'kl', 'ˌ', 'st', 'kw', 'j', 'ts', 'kˌ', 'nj', 'ms', 'nˌ', 'bw', 'ɦj', 'dˌ', 'r', 'pw', 'fɾˌ', 'ɾ', 'sj', 'ʃ', 'gw', 'tsˌ', 'cr', 'b', 'nw', 'plˌ', 'ɾˌ', 'kj', 'lw', 'pɾj', 'n', 'fɾj', 'vˌ', 'gl', 'tl']
@@ -47,5 +47,5 @@ def tratar_fonemas(divisao_fonetica):
             if fonema.startswith(elem):
                 fonema = fonema.replace(elem, '')
                 continue
-        divisao_fonetica_tratada = divisao_fonetica_tratada + ' ' + fonema_simple(fonema)
+        divisao_fonetica_tratada.append(fonema_simple(fonema))
     return divisao_fonetica_tratada
