6db6cd4288fde8aa7eb13936f6e1e8dbe2b1c23c
aaavalenca <32614967+aaavalenca@users.noreply.github.com>
2023-11-17 20:30:40 -0300

    made public

--- Scrapper/Server/app.py
+++ Scrapper/Server/app.py
@@ -13,4 +13,5 @@ def rhyme(word):
 #     return "hello"
 
 if __name__ == "__main__":
-    app.run(port=8000, debug=True)
+    app.run(debug=False, host='0.0.0.0')
+    # app.run(port=8000, debug=True)
